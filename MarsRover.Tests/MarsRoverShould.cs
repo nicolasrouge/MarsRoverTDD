ac6017385fdff4bf7431aac8e004590991026b23
Nicolas Rouge <nicolas.rouge777@gmail.com>
2023-12-27 18:48:04 +0000

    simplify unit test

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -4,44 +4,26 @@ namespace MarsRover.Tests;
 
 public class MarsRoverShould
 {
-
     /// <summary>
-    /// Test to ensure that we can initialize the rover with different states.
-    /// </summary>
-    /// <param name="initialState"></param>
-    [Theory]
-    [InlineData("0:0:N")]
-    [InlineData("1:1:S")]
-    public void PrintInitialState(string initialState)
-    {
-        //Arrange
-        var marsRover = new MarsRover(initialState);
-
-        //Act
-        var result = marsRover.Execute("");
-
-        //Assert
-        result.Should().Be(initialState);
-    }
-
-    /// <summary>
-    /// Move the Rover forward in every direction.
+    /// Test to ensure the correct execution of the commands.
     /// </summary>
     /// <param name="initialState"></param>
     /// <param name="expectedResult"></param>
     [Theory]
-    [InlineData("0:0:N", "0:1:N")]
-    [InlineData("1:1:N", "1:2:N")]
-    [InlineData("1:1:W", "0:1:W")]
-    [InlineData("1:1:E", "2:1:E")]
-    [InlineData("1:1:S", "1:0:S")]
-    public void MoveForward(string initialState, string expectedResult)
+    [InlineData("0:0:N", "", "0:0:N")]
+    [InlineData("1:1:S", "", "1:1:S")]
+    [InlineData("0:0:N", "M", "0:1:N")]
+    [InlineData("1:1:N", "M", "1:2:N")]
+    [InlineData("1:1:W", "M", "0:1:W")]
+    [InlineData("1:1:E", "M", "2:1:E")]
+    [InlineData("1:1:S", "M", "1:0:S")]
+    public void ExecuteCommands(string initialState, string commands, string expectedResult)
     {
         //Arrange
         var marsRover = new MarsRover(initialState);
 
         //Act
-        var result = marsRover.Execute("M");
+        var result = marsRover.Execute(commands);
 
         //Assert
         result.Should().Be(expectedResult);
